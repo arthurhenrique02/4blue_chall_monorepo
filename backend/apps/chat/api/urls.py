46d29767d839ee325303a94baecb9429eb6956e6
arthurhenrique02 <profissionalarthurhenrique@gmail.com>
2025-11-24 18:59:45 -0300

    chore: alterado nome da api de menssagens

--- backend/apps/chat/api/urls.py
+++ backend/apps/chat/api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from apps.chat.api.viewsets import ChatsHistoryReadOnlyViewSet, MessageReadCreateViewSet
+from apps.chat.api.viewsets import ChatsHistoryReadOnlyViewSet, MessageCreateViewSet
 
 router = DefaultRouter()
 
 router.register(r"history", ChatsHistoryReadOnlyViewSet, basename="chats-history")
-router.register(r"messages", MessageReadCreateViewSet, basename="messages")
+router.register(r"messages", MessageCreateViewSet, basename="messages")
 
 urlpatterns = [
     path("", include(router.urls)),
